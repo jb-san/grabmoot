779fe6442a3777299ef066c76b6d9c4f07066d83
enroxorz <enrique.j.matta@gmail.com>
2012-06-09 17:15:40 -0400

    Timeout issues are handled differently now

--- lib/grabmoot.rb
+++ lib/grabmoot.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'openssl'
+require 'timeout'
 # The main GrabMoot Class
 class GrabMoot
   # GrabMoot will be able to download all images from any thread passed in
@@ -14,20 +15,25 @@ class GrabMoot
   def self.down_thread(url)
     begin
       puts "Hey Anon! Hope this helps."
-      doc = Nokogiri::HTML(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
-      doc.xpath("//a[@class='fileThumb']").each do |link|
-        img_url = "http:#{link.attributes['href'].to_s}"
-        open(img_url) {|f|
-          File.open("#{File.basename(img_url)}","wb") do |file|
-            file.puts f.read
-          end
-        }
+      timeout(120) do
+        doc = Nokogiri::HTML(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+        doc.xpath("//a[@class='fileThumb']").each do |link|
+          img_url = "http:#{link.attributes['href'].to_s}"
+          open(img_url) {|f|
+            File.open("#{File.basename(img_url)}","wb") do |file|
+              file.puts f.read
+            end
+          }
+        end
       end
       puts "Finished!"
+    rescue Timeout::Error
+      puts "So an image timed out. Could be network, could be a dead thread.\n"
+      puts "Try it again"
     rescue Exception => e
       puts "You probably forgot the URL from the thread you need.\n"
       puts "If you did, then check your internet connection\n"
-      puts "If you did both, email this to me\n"
+      puts "If you did both, put a ticket in github\n"
       puts e.message
       puts e.backtrace
     end
